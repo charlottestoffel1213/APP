a06aa8e21ebbf5baa383e20d5e9da0c8da4a3b57
Idriss BELAID <Idriss BELAID>
2016-12-12 15:37:45 +0100

    capteurs idriss  en php

---
+++ Capteur1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<title>Lean On : Here for you !</title>
+</head>
+<body>
+	<img class="logo" alt="logo du site" src="images/logo.png">
+			<nav class= "menu">
+					<ul>
+						<li><a href="Accueil.html">Accueil</a></li>
+						<li><a href="Ma maison.html">Ma maison</a></li>
+						<li><a href="Mon compte.html">Mon compte</a></li>
+						<li><a href="Ma boutique.html">Ma boutique</a></li>
+						<li><a href="Nous contacter.html">Nous contacter</a></li>
+					</ul>
+			</nav>
+	<section>
+		<nav>
+			
+		</nav>
+		<h1 class=Capteur1>Capteur 1</h1>
+		<table>
+			<caption>Paramètres du Capteur 1 </caption>
+			<tr>
+				<th>Valeur</th>
+				<th>Changer maintenant</th>
+				<th>Programmer</th>
+				<th>Valeur Conseillée</th>
+			</tr>
+			<tr>
+				<td>X</td>
+				<td></td>
+				<td></td>
+				<td>Y</td>
+			</tr>
+		</table>
+	</section>
+
+
+
+
+</body>
+<footer>
+		<ul>
+			<li><a href="Ma boutique.html">Conditions générales d'utilisation</a></li>
+			<li><a href="Nous contacter.html">Nous contacter</a></li>
+		</ul>
+		
+</footer>
+</html>
