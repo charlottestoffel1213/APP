49b19d1debbef14bdca75436885136d5f750eccf
teteo14 <teteo1403@gmail.com>
2016-12-12 15:22:09 +0100

    test theo

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page d'Insciption</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+ <?php echo "test" ?> 
+
+    </body>
+</html>
